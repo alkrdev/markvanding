995035d7141d1f9d5e35c6ec7dd068dd3e642b5d
unknown <johansen0508@gmail.com>
2021-03-12 14:20:39 +0100

    Approutes for functions

--- Markvandingserver/routes/approutes.js
+++ Markvandingserver/routes/approutes.js
@@ -1,6 +1,44 @@
 module.exports = (app) => {
-    var machines = require("../controllers/machineController")
+    var machines = require("../controllers/machineController.js")
+    var pumps = require("../controllers/pumpController.js")
+    var sms = require("../controllers/smsController.js")
 
     app.route("/inactivemachines")
         .get(machines.getInactiveMachines)
+
+    app.route("/activemachines")
+        .get(machines.getActiveMachines)
+
+    app.route("/allmachines")
+        .get(machines.getAllMachines)
+
+    app.route("/updatemachine")
+        .post(machines.updateMachineById)
+
+    app.route("/removemachine")
+        .post(machines.removeMachineById)
+    
+    app.route("/createmachine")
+        .post(machines.createMachine)
+
+    app.route("/inactivepumps")
+        .get(pumps.getInactivePumps)
+
+    app.route("/activepumps")
+        .get(pumps.getActivePumps)
+
+    app.route("/allpumps")
+        .get(pumps.getAllPumps)
+
+    app.route("/updatepump")
+        .post(pumps.updatePumpById)
+
+    app.route("/removepump")
+        .post(pumps.removePumpById)
+    
+    app.route("/createpump")
+        .post(pumps.createPump)
+
+    app.route("/sendsms")
+        .post(sms.sendSingleMessage)
 }
