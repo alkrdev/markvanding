ea059ccd0e2fd6f999f7b21c08225a1283421a49
unknown <johansen0508@gmail.com>
2021-03-12 14:17:57 +0100

    Get inactive machines added

--- Markvandingserver/models/machineModel.js
+++ Markvandingserver/models/machineModel.js
@@ -8,7 +8,17 @@ class Machine{
     }
 
     static getInactiveMachines(result){
-        sql.query("SELECT", (err, res) => {
+        sql.query("SELECT * FROM markvanding.machines WHERE active = 0", (err, res) => {
+            if(err){
+                console.log("Error: ", err)
+                result(err, null)
+            }
+            else{
+                result(null, res)
+            }
+        })
+    }
+
     static getActiveMachines(result){
         sql.query("SELECT * FROM markvanding.machines WHERE active = 1", (err, res) => {
             if(err){
