9f9d7ea0101d93a3bcbadcc55b89a4d62e504d56
unknown <johansen0508@gmail.com>
2021-03-12 14:16:16 +0100

    Get active machine added

--- Markvandingserver/controllers/machineController.js
+++ Markvandingserver/controllers/machineController.js
@@ -11,6 +11,14 @@ exports.getInactiveMachines = (req, res) => {
     })
 }
 
+exports.getActiveMachines = (req, res) => {
+    Machine.getActiveMachines((err, machines) => {
+        if(err){
+            res.send(err)
+        }
+        res.send(machines)
+    })
+}
 exports.updateMachineById = (req, res) => {
     Machine.updateMachineById(req.body.id, new Machine(req.body), (err, machines) => {
         if(err){
--- Markvandingserver/models/machineModel.js
+++ Markvandingserver/models/machineModel.js
@@ -9,6 +9,17 @@ class Machine{
 
     static getInactiveMachines(result){
         sql.query("SELECT", (err, res) => {
+    static getActiveMachines(result){
+        sql.query("SELECT * FROM markvanding.machines WHERE active = 1", (err, res) => {
+            if(err){
+                console.log("Error: ", err)
+                result(err, null)
+            }
+            else{
+                result(null, res)
+            }
+        })
+    }
             if(err){
                 console.log("Error: ", err)
                 result(err, null)
