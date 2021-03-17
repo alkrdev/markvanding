c652e0803abba6950cc5aad3bf3b0c3d9805ce4f
JJG4ming <johansen0508@gmail.com>
2021-03-17 15:57:56 +0100

    Added stop watering feature and changed some small things

--- src/components/overview.jsx
+++ src/components/overview.jsx
@@ -1,27 +1,38 @@
 import React, { useEffect, useState } from 'react';
 
-function Overview({activeMachines, activePumps}) {
+function Overview({activeMachines, activePumps, stopMachine, stopPump}) {
+
+  function sendStopSMS(pumpnumber, pumpstopcode){
+    fetch("http://10.10.60.161:5000/sendsms", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ 
+        number: pumpnumber,
+        message: pumpstopcode
+      })
+    })
+  }
 
   useEffect(function() 
   {
   }, [])
 
   return (
-    <table id="overview">
+    <table id="overview" className="sortable">
       <colgroup>
         <col style={{width: "10%"}}></col>
-        <col style={{width: "20%"}}></col>
-        <col style={{width: "40%"}}></col>
-        <col style={{width: "15%"}}></col>
-        <col style={{width: "5%"}}></col>
+        <col style={{width: "24%"}}></col>
+        <col style={{width: "45%"}}></col>
+        <col style={{width: "10%"}}></col>
       </colgroup>
       <thead>
         <tr>
           <th>Maskine nr.</th>
           <th>Pumpe Navn</th>
           <th>Færdig</th>
-          <th>Status</th>
-          <th>Stop</th>
+          <th>Stop Vanding</th>
         </tr>
       </thead>
       <tbody>
@@ -41,14 +52,21 @@ function Overview({activeMachines, activePumps}) {
               <td>{machine["id"]}</td>
               <td>{machine["pumpname"]}</td>
               <td>{time}</td>
-              <td>"status"</td>
-              <td>
-                <div data-machineid={machine.id} data-pumpid={pump?.id} onClick={(event) => {
-                  const { machineid, pumpid } = event.target.dataset
+              <td id="stopwateringbutton" onClick={(event) => {
 
-                  // IT NEVER HAPPENED
-                }}></div>
-              </td>
+                  var answer = window.confirm("Hvis du vil stoppe vanding tryk OK")
+                  
+                  if (!answer === true) return;
+
+                  //var pumpnumber = "+45" + pump.number
+                  //var pumpstopcode = pump.stopcode
+                  //sendStopSMS(pumpnumber, pumpstopcode)
+                  
+                  stopMachine(machine)
+                  stopPump(pump)
+                  window.location.href="/"
+                }}>
+              <p id="stopwateringbuttontext">S</p></td>
             </tr>
           )
         })}
