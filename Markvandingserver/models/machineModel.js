89b0b2753e3f70451a8ccd60cc6e76fcdb6c8ae4
JJG4ming <johansen0508@gmail.com>
2021-03-17 15:56:25 +0100

    Only fetch still active machines. Machines that are done are changed to inactive

--- Markvandingserver/models/machineModel.js
+++ Markvandingserver/models/machineModel.js
@@ -20,13 +20,35 @@ class Machine{
     }
 
     static getActiveMachines(result){
-        sql.query("SELECT * FROM markvanding.machines WHERE active = 1", (err, res) => {
+        sql.query("SELECT * FROM markvanding.machines WHERE active = 1 ORDER BY time ASC", (err, res) => {
             if(err){
                 console.log("Error: ", err)
                 result(err, null)
             }
             else{
-                result(null, res)
+                var stillgoing = res.filter(x => new Date() < new Date(x.time))
+                var expired = res.filter(x => new Date() > new Date(x.time))
+
+                if (expired.length > 0) {
+                    expired.forEach(element => {
+                        sql.query("UPDATE markvanding.pumps SET active = 0 WHERE name = ?", element.pumpname, (err, res) => {
+                            if(err){
+                                console.log("Error: ", err)
+                                result(err, null)
+                            }
+                        })
+                        element.active = 0
+                        element.time = null
+                        element.pumpname = null
+                        sql.query("UPDATE markvanding.machines SET ? WHERE id = ?", [element, element.id], (err, res) => {
+                            if(err){
+                                console.log("Error: ", err)
+                                result(err, null)
+                            }
+                        })
+                    });
+                }
+                result(null, stillgoing)
             }
         })
     }
@@ -60,8 +82,7 @@ class Machine{
 
     static removeMachineById(id, result){
         console.log(id)
-        console.log(result)
-        sql.query("REMOVE FROM markvanding.machines WHERE id = ?", id, (err, res) => {
+        sql.query("DELETE FROM markvanding.machines WHERE id = ?", id, (err, res) => {
             if(err){
                 console.log("Error: ", err)
                 result(err, null)
@@ -74,7 +95,6 @@ class Machine{
 
     static createMachine(createdMachine, result){
         console.log(createdMachine)
-        console.log(result)
         sql.query("INSERT INTO markvanding.machines SET ?", createdMachine, (err, res) => {
             if(err){
                 console.log("Error: ", err)
