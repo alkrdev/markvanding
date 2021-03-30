3444d75a3d8bd149c9977ff22911979cf0bb1991
JJG4ming <johansen0508@gmail.com>
2021-03-30 15:56:16 +0200

    Added slide feature to maintenance page

--- src/components/maintenance.jsx
+++ src/components/maintenance.jsx
@@ -26,22 +26,48 @@ function Maintenance({allMachines, setStage, setShownMachine, setNotes}){
     <div id="maintenance">
       <h1 id="maintenancetext">Tryk på en boks for at tilgå maskinen</h1>
       {allMachines.map((machine) => {
+        var color
         var active
+        var pumpname
+        var time
         if(machine.active == 1) {
           active = "Aktiv"
+          pumpname = machine.pumpname
+          time = new Date(machine["time"]).toLocaleString("da-DK", {
+            dateStyle: "medium",
+            timeStyle: "short"
+        });
         }
         else{
           active = "Inaktiv"
+          pumpname = "Ingen pumpe"
+          time = "Ingen tid"
         }
       return(
-        <button className="maintenanceboxes" onClick={() => {HandleClick(machine)}}>
+        <button id="maintenanceboxes" onClick={() => {HandleClick(machine)}}>
             <div>
               <h1 id="machineid">
                 {machine.id}
               </h1>
-              <p id="active">
-                {active}
-              </p>
+              <div id="hid-box">
+                <p id="active">
+                  {active}
+                </p>
+                <div id="maintenanceboxhover">
+                  <p className="maintenanceboxheading">
+                    Pumpenavn
+                  </p>
+                  <h3 className="maintenanceboxtext">
+                    {pumpname}
+                  </h3>
+                  <p className="maintenanceboxheading">
+                    Stop tidspunkt
+                  </p>
+                  <h3 className="maintenanceboxtext">
+                    {time}
+                  </h3>
+                </div>
+              </div>
             </div>
         </button>
       )
