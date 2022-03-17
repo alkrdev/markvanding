fdaaf9b040bbbcbc8096b74be0a81ef4d8884cfd
Jakob Johansen <jako7614@edu.sde.dk>
2022-03-17 15:47:45 +0100

    Database and props added

--- pages/machinepark.jsx
+++ pages/machinepark.jsx
@@ -16,29 +16,36 @@ import DeletePump from "../components/DeletePump"
 
 const MachinePark = () => {
   const [currentMachine, setCurrentMachine] = useState({})
-  const [showingModal, setShowingModal] = useState(false)
+  const [showingModal, setShowingModal] = useState({pump: false, createpump: false, deletepump: false, machine: false, createmachine: false, deletemachine: false})
   const [currentPump, setCurrentPump] = useState({
     name: "",
     number: "",
     startcode: "",
     stopcode: ""
   })
+  const [pumps, setPumps] = useState([]) 
+  const [machines, setMachines] = useState([])
+
+  useEffect(() => {
+    fetch("/api/pumps").then(res => res.json()).then(json => setPumps(json))
+    fetch("/api/machines").then(res => res.json()).then(json => setMachines(json))
+  }, [])
 
   return (
     <div>
       <Header />
-      <Pumps setCurrentPump={setCurrentPump} setShowingModal={setShowingModal}/>
-      <Machines setCurrentMachine={setCurrentMachine} setShowingModal={setShowingModal}/>
+      <Pumps setCurrentPump={setCurrentPump} setShowingModal={setShowingModal} showingModal={showingModal} pumps={pumps}/>
+      <Machines setCurrentMachine={setCurrentMachine} setShowingModal={setShowingModal} showingModal={showingModal} machines={machines}/>
 
-      {showingModal ? <React.Fragment>
-        <CreatePump    setShowingModal={setShowingModal} />
-        <UpdatePump    setShowingModal={setShowingModal} currentPump={currentPump}/>
-        <DeletePump    setShowingModal={setShowingModal} currentPump={currentPump}/>
+      <React.Fragment>
+        {showingModal.createpump ? <CreatePump    setShowingModal={setShowingModal} showingModal={showingModal} setPumps={setPumps} pumps={pumps}/> : <></>}
+        {showingModal.pump ? <UpdatePump    setShowingModal={setShowingModal} currentPump={currentPump} showingModal={showingModal} pumps={pumps} setPumps={setPumps}/> : <></>}
+        {showingModal.deletepump ? <DeletePump    setShowingModal={setShowingModal} currentPump={currentPump} showingModal={showingModal} setPumps={setPumps} pumps={pumps}/> : <></>}
 
-        <CreateMachine setShowingModal={setShowingModal} />
-        <UpdateMachine setShowingModal={setShowingModal} currentMachine={currentMachine}/>
-        <DeleteMachine setShowingModal={setShowingModal} currentMachine={currentMachine}/>
-      </React.Fragment> : <></>}
+        {showingModal.createmachine ? <CreateMachine setShowingModal={setShowingModal} showingModal={showingModal} machines={machines} setMachines={setMachines} /> : <></>}
+        {showingModal.machine ? <UpdateMachine setShowingModal={setShowingModal} currentMachine={currentMachine} showingModal={showingModal} machines={machines} setMachines={setMachines}/> : <></>}
+        {showingModal.deletemachine ? <DeleteMachine setShowingModal={setShowingModal} currentMachine={currentMachine} showingModal={showingModal} machines={machines} setMachines={setMachines}/> : <></>}
+      </React.Fragment>
     </div>
   )
 }
