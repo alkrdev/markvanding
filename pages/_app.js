f07cc2aa328d519a0a6cd3e6829eae7aa92241c5
Alexander Kristensen <ak@kk-partner.dk>
2022-05-12 08:46:00 +0200

    Faux-middleware to handle missing time

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,32 @@
 import '../styles/globals.css'
 import '../styles/Home.css'
 
-// import { Auth0Provider } from "@auth0/auth0-react"
-
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Component {...pageProps} />
+  )
+}
+
+MyApp.getInitialProps = async ({ Component, router, ctx }) => {
+  var url = process.env.NEXT_PUBLIC_BASE_URL + "/api/machines";
+  console.log(url)
+  const response = await fetch(url)
+  const data = await response.json()
+
+  var machineWithoutTime = data.find(machine => machine.pumpname && machine.time == null)
+  
+  if (ctx.res && !ctx.pathname.startsWith("/choosetime")) {
+    if (machineWithoutTime != undefined) {
+      ctx.res.writeHead(301, {
+        Location: '/choosetime/' + machineWithoutTime.id
+      });
+      ctx.res.end();
+    }
+  }
+
+  return {
+    props: {}, // will be passed to the page component as props
+  }
 }
 
 export default MyApp
